ac4676acf1055a3ece9643a5eb004df2e82cdfd5
Lenzlee Wheeler <lwheeler@luc.edu>
2023-10-02 17:10:15 -0500

    added spec.js file for testing

---
+++ shapes.spec.js
@@ -0,0 +1,29 @@
+const {Circle, Square, Triangle} = require("./shapes")
+
+// Circle
+describe('Circle', () => {
+    test('renders correctly', () => {
+      const shape = new Circle();
+      var color =('blue')
+      shape.setColor(color);
+      expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}">`);
+    });
+  });
+  // Square
+  describe('Square', () => {
+      test('renders correctly', () => {
+        const shape = new Square();
+        var color =('green')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<rect x="50" height="200" width="200" fill="${color}">`);
+      });
+    });
+  // Triangle
+  describe('Triangle', () => {
+    test('renders correctly', () => {
+      const shape = new Triangle();
+      var color =('pink')
+      shape.setColor(color);
+      expect(shape.render()).toEqual(`<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${color}">`);
+    });
+  });
